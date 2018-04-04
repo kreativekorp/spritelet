7579ab5044fdbe692ca4c82c388e30ac3628e6df
Rebecca G. Bettencourt <beckiejo@gmail.com>
2018-04-04 02:42:40 -0700

    Support for GCI format

--- spritelet2/spritelet_bitmap.cpp
+++ spritelet2/spritelet_bitmap.cpp
@@ -8,6 +8,36 @@
 extern ST7735 tft;
 extern FATFS fs;
 
+static uint16_t tft_blit(uint16_t ptr, int16_t x, int16_t y, int16_t w, int16_t h) {
+	uint32_t n;
+	uint16_t rem;
+	n   = w;
+	n  *= h;
+	n <<= 1;
+	if (n) {
+		tft.setAddrWindow(x, y, x + w - 1, y + h - 1);
+		rem = 512 - ptr;
+		if (rem > n) rem = n;
+		TFT_DC_PORT |=  TFT_DC_MASK;
+		TFT_CS_PORT &=~ TFT_CS_MASK;
+		SPI.writeBlock(fs.buf + ptr, rem);
+		TFT_CS_PORT |=  TFT_CS_MASK;
+		n -= rem;
+		ptr += rem;
+		while (n && !fs.read()) {
+			rem = 512;
+			if (rem > n) rem = n;
+			TFT_DC_PORT |=  TFT_DC_MASK;
+			TFT_CS_PORT &=~ TFT_CS_MASK;
+			SPI.writeBlock(fs.buf, rem);
+			TFT_CS_PORT |=  TFT_CS_MASK;
+			n -= rem;
+			ptr = rem;
+		}
+	}
+	return ptr;
+}
+
 uint8_t tft_drawSMF(char * path, uint8_t (*callback)(void)) {
 	uint32_t time = millis();
 	int16_t bx = 0, by = 0;
@@ -26,30 +56,7 @@ uint8_t tft_drawSMF(char * path, uint8_t (*callback)(void)) {
 				y = fs.buf[ptr++];
 				w = fs.buf[ptr++];
 				h = fs.buf[ptr++];
-				n   = w;
-				n  *= h;
-				n <<= 1;
-				if (n) {
-					tft.setAddrWindow(bx + x, by + y, bx + x + w - 1, by + y + h - 1);
-					clr = 512 - ptr;
-					if (clr > n) clr = n;
-					TFT_DC_PORT |=  TFT_DC_MASK;
-					TFT_CS_PORT &=~ TFT_CS_MASK;
-					SPI.writeBlock(fs.buf + ptr, clr);
-					TFT_CS_PORT |=  TFT_CS_MASK;
-					n -= clr;
-					ptr += clr;
-					while (n && !fs.read()) {
-						clr = 512;
-						if (clr > n) clr = n;
-						TFT_DC_PORT |=  TFT_DC_MASK;
-						TFT_CS_PORT &=~ TFT_CS_MASK;
-						SPI.writeBlock(fs.buf, clr);
-						TFT_CS_PORT |=  TFT_CS_MASK;
-						n -= clr;
-						ptr = clr;
-					}
-				}
+				ptr = tft_blit(ptr, bx + x, by + y, w, h);
 			} else if (cmd == 'F') {
 				x = fs.buf[ptr++];
 				y = fs.buf[ptr++];
@@ -102,4 +109,37 @@ uint8_t tft_drawSMF(char * path, uint8_t (*callback)(void)) {
 	}
 	fs.close();
 	return SMF_OK;
+}
+
+uint8_t tft_drawGCI(uint32_t sect, uint16_t offs, int16_t x, int16_t y, uint8_t (*callback)(void)) {
+	uint32_t time = millis();
+	uint16_t ptr = offs;
+	int16_t w, h;
+	uint16_t ms, frames, i;
+	uint8_t ret;
+	if (fs.seek(sect) || fs.read()) return SMF_ERROR;
+	w = fs.buf[ptr++]; w <<= 8; w |= fs.buf[ptr++]; if (!w) return SMF_ERROR;
+	h = fs.buf[ptr++]; h <<= 8; h |= fs.buf[ptr++]; if (!h) return SMF_ERROR;
+	if (fs.buf[ptr++] != 0x10) return SMF_ERROR;
+	if ((ms = fs.buf[ptr++])) {
+		frames = fs.buf[ptr++]; frames <<= 8; frames |= fs.buf[ptr++];
+		if (!frames) return SMF_ERROR;
+	} else {
+		frames = 0;
+	}
+	if (frames < 2) {
+		tft_blit(ptr, x, y, w, h);
+		return SMF_OK;
+	} else {
+		for (;;) {
+			for (i = 0; i < frames; i++) {
+				ptr = tft_blit(ptr, x, y, w, h);
+				do { if (callback && (ret = callback())) return ret; }
+				while ((millis() - time) < ms);
+				time = millis();
+			}
+			if (fs.seek(sect) || fs.read()) return SMF_ERROR;
+			ptr = offs + 8;
+		}
+	}
 }
--- spritelet2/spritelet_bitmap.h
+++ spritelet2/spritelet_bitmap.h
@@ -7,5 +7,6 @@
 #define SMF_ERROR 0xFF
 
 uint8_t tft_drawSMF(char * path, uint8_t (*callback)(void));
+uint8_t tft_drawGCI(uint32_t sect, uint16_t offs, int16_t x, int16_t y, uint8_t (*callback)(void));
 
 #endif
